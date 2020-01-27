764d9d20321da08d012fe5760429573a79d104d2
Charles Pelton <charliejp0311@gmail.com>
2020-01-27 18:24:11 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,17 +2,19 @@
 def prime?(number)
   prm = false
   if number > 1
-    return true
-  elsif number > 2
-    test_numbers = [2...(number - 1)]
-    pp test_numbers
-    count = 0
-    while count < test_numbers.length
-      #pp number % test_numbers[count]
-      if number % test_numbers[count] == 0
-        return false
+    if number > 2
+      test_numbers = [2...(number - 1)]
+      pp test_numbers
+      count = 0
+      while count < test_numbers.length
+        #pp number % test_numbers[count]
+        if number % test_numbers[count] == 0
+          return false
+        end
+        count += 1
       end
-      count += 1
+    else
+      return true
     end
   else
     return false
