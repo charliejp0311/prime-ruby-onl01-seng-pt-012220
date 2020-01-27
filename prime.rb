ae496539f3dabf2eb70c510d4ea4bae889349157
Charles Pelton <charliejp0311@gmail.com>
2020-01-27 18:08:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -7,10 +7,10 @@ def prime?(number)
     test_numbers = [2...(number - 1)]
     count = 0
     while count < test_numbers.length
-      pp number % test_numbers[count]
-      #if number % test_numbers[count] == 0
-      #  prm = false
-      #end
+      #pp number % test_numbers[count]
+      if number % test_numbers[count] == 0
+        prm = false
+      end
       count += 1
     end
   end
