fb4c3dc4a6723cc3347ba46e00088ae1463d1d0b
Charles Pelton <charliejp0311@gmail.com>
2020-01-27 15:34:01 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,17 @@
 # Add  code here!
 def prime?(number)
-  divisible = [1..number]
+  divisible = [1..(number - 1)]
   i = 0
+  prm = true
   while i < divisible.length
-    if i % divisible[i]
-      return false
+    if i % divisible[i] == 0
+      prm = false
+      break
     else
       i += 1
     end
 
   end
-
+  prm
 
 end
