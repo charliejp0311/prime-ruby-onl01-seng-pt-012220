20c7347b4cc3f361075eafe01cbf86318e2cbe96
Charles Pelton <charliejp0311@gmail.com>
2020-01-27 19:37:31 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
 # Add  code here!
+def prime?(number)
+  #prm = false
+  if number > 1
+      test_numbers = (1..number).to_a
+      answers = []
+      count = 0
+      while count < test_numbers.length
+        if number % test_numbers[count] == 0
+          answers << test_numbers[count]
+        end
+        count += 1
+      end
+      if answers.length == 2
+        return true
+      else
+        return false
+      end
+  else
+    return false
+  end
+  #prm
+
+end
