51ee44953fcba54db56ebc758e316a85884b3e18
Charles Pelton <charliejp0311@gmail.com>
2020-01-27 16:58:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -8,7 +8,7 @@ def prime?(number)
     test_numbers = [2 .. (number)]
     test_numbers.each do |num|
       binding.pry
-      num % number
+      pp num % number
         #prm = false
     end
   end
