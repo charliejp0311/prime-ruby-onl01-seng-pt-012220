0d14f40ba669baf022e6e3d37de5ac3dedc5618d
Charles Pelton <charliejp0311@gmail.com>
2020-01-27 16:10:14 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,25 +1,11 @@
 # Add  code here!
 def prime?(number)
   prm = true
-  if number < 0
+  if number < 0 || number == 0
     prm = false
-    return prm
-  elsif number == 0 || number == 1
-    prm = false
-    return prm
-  elsif number == 2 || number == 3
     return prm
   else
-    divisible = [1..(number-1)]
-    i = 0
-    divisible.each do |num|
-      if (i + 1) % num == 0
-        prm = false
-        return prm
-      else
-        i += 1
-      end
-    end
+    
     prm
   end
 end
