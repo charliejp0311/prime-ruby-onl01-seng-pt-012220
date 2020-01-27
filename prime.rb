acb8bf18246dc35137cb5d8f8f8b02d9a20754df
Charles Pelton <charliejp0311@gmail.com>
2020-01-27 17:20:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,4 @@
 # Add  code here!
-require "pry"
 def prime?(number)
   prm = false
   if number > 1
@@ -7,10 +6,10 @@ def prime?(number)
   elsif number > 2
     test_numbers = [2...(number - 1)]
     test_numbers.each do |num|
-      binding.pry
       if number % num == 0
         prm = false
       end
+      prm 
     end
   end
   prm
